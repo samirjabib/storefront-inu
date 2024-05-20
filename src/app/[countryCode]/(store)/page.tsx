fce222ca4c32c94cf5b912f723b76adf7183d9de
samirjabib <samirdev9669@gmail.com>
2024-05-20 18:40:40 -0500

    Make promises

--- src/app/[countryCode]/(store)/page.tsx
+++ src/app/[countryCode]/(store)/page.tsx
@@ -1,25 +1,26 @@
-import Banner from '@/components/common/banner/banner';
-import { dummieData } from '@/components/common/banner/data';
-import ProductVitrine from '@/components/common/products/product-vitrine/product-vitrine';
-import BannerGrid from '@/components/home/banner-grid/banner-grid';
-import FilterCategories from '@/components/home/filter-categories/filter-categories';
-import FlashSalesGrid from '@/components/home/flash-sales-grid/flash-sales-grid';
-import WelcomeHome from '@/components/home/welcome/welcome-home';
-import { collection_mock, Product_Mock } from '@/lib/mocks/mocks';
+import Banner from "@/components/common/banner/banner";
+import { dummieData } from "@/components/common/banner/data";
+import ProductVitrine from "@/components/common/products/product-vitrine/product-vitrine";
+import BannerGrid from "@/components/home/banner-grid/banner-grid";
+import FilterCategories from "@/components/home/filter-categories/filter-categories";
+import FlashSalesGrid from "@/components/home/flash-sales-grid/flash-sales-grid";
+import Promises from "@/components/home/promises/promises";
+import WelcomeHome from "@/components/home/welcome/welcome-home";
+import { collection_mock, Product_Mock } from "@/lib/mocks/mocks";
 
 export default function Home() {
-	return (
-		<main className='bg-neutral-100'>
-			<FilterCategories />
-			<Banner data={dummieData} />
-			<WelcomeHome />
-			<ProductVitrine title='Lo mas vendido' />
-			<BannerGrid />
-			<FlashSalesGrid
-				collection={collection_mock as unknown as Product_Mock[]}
-			/>
-
-			<ProductVitrine title='Lo nuevo' />
-		</main>
-	);
+  return (
+    <main className="bg-neutral-100">
+      <FilterCategories />
+      <Banner data={dummieData} />
+      <WelcomeHome />
+      <ProductVitrine title="Lo mas vendido" />
+      <BannerGrid />
+      <FlashSalesGrid
+        collection={collection_mock as unknown as Product_Mock[]}
+      />
+      <ProductVitrine title="Lo nuevo" />
+      <Promises />
+    </main>
+  );
 }
--- src/components/home/promises/promises.tsx
+++ src/components/home/promises/promises.tsx
@@ -1,8 +1,27 @@
+import { Package } from "lucide-react";
 import { MdLocalShipping } from "react-icons/md";
 import { RiSecurePaymentFill } from "react-icons/ri";
 
 export default function Promises() {
   return (
-    <div className="flex flex-row max-w-screen-2xl px-4 md:px-6 mx-auto"></div>
+    <section className="flex flex-row max-w-screen-2xl px-4 md:px-6 mx-auto py-12 bg-blue-100 rounded-lg">
+      <div className="grid grid-cols-1 gap-5 w-full md:grid-cols-5">
+        {Array.from({ length: 5 }).map((item) => {
+          return (
+            <div className="flex items-row items-center flex-row gap-x-4 border border-gray-200 w-full p-3 rounded-lg bg-neutral-50">
+              <div className="bg-blue-200 rounded-full shadow-xs w-12 h-12 border border-blue-300 flex items-center justify-center">
+                <Package size={24} className="text-gray-700" />
+              </div>
+              <div>
+                <h3 className="text-xs text-gray-800 font-semibold">
+                  Envío gratis en medellin Colombia
+                </h3>
+                <p className="text-xs text-gray-500">Entrega en 24h hábiles</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 }

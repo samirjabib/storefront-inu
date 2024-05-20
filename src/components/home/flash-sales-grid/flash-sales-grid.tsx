a66919279005871e87c4769e8749714b8c2fae50
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-05-20 13:28:19 -0500

    flash

--- src/components/home/flash-sales-grid/flash-sales-grid.tsx
+++ src/components/home/flash-sales-grid/flash-sales-grid.tsx
@@ -1,3 +1,4 @@
+import LocalizedClientLink from '@/components/common/localized-client-link/localized-client-link';
 import { ProductCard } from '@/components/common/products/common/product-card/product-card';
 import { Product_Mock } from '@/lib/mocks/mocks';
 
@@ -12,14 +13,22 @@ export default function FlashSalesGrid({
 				⚡Ventas Flash
 			</h2>
 			<section className='flex w-full gap-4'>
-				<div className='grid grid-cols-1 lg:grid-cols-2 w-full lg:w-1/2 bg-brand-100 p-6 rounded-lg gap-6 '>
-					{collection.slice(0, 4).map(product => (
-						<ProductCard
-							containerStyles='max-w-full'
-							product={product}
-							key={product.id}
-						/>
-					))}
+				<div className='bg-brand-100 p-6 rounded-lg flex flex-col w-1/2'>
+					<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mb-4'>
+						{collection.slice(0, 4).map(product => (
+							<ProductCard
+								containerStyles='max-w-full'
+								product={product}
+								key={product.id}
+							/>
+						))}
+					</div>
+					<LocalizedClientLink
+						href=''
+						className='text-sm font-semibold text-end hover:underline'
+					>
+						Ver todo
+					</LocalizedClientLink>
 				</div>
 				<section className='w-1/2 flex flex-col gap-6'>
 					<div className='w-full h-1/2 bg-gray-200 rounded-lg'></div>

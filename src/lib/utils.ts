e920af96af5e0d3a96f5a967d4b8ff02037c30d5
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-05-21 14:43:42 -0500

    add

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { Region } from '@medusajs/medusa';
-import { PricedProduct } from '@medusajs/medusa/dist/types/pricing';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
@@ -28,53 +26,3 @@ export function shortenProductName(
 
 	return productName;
 }
-
-/* 
-
----
-*/
-
-const transformProductPreview = (
-	product: PricedProduct,
-	region: Region
-): ProductPreviewType => {
-	const variants = product.variants as unknown as CalculatedVariant[];
-
-	let cheapestVariant = undefined;
-
-	if (variants?.length > 0) {
-		cheapestVariant = variants.reduce((acc, curr) => {
-			if (acc.calculated_price > curr.calculated_price) {
-				return curr;
-			}
-			return acc;
-		}, variants[0]);
-	}
-
-	return {
-		id: product.id!,
-		title: product.title!,
-		handle: product.handle!,
-		thumbnail: product.thumbnail!,
-		created_at: product.created_at,
-		price: cheapestVariant
-			? {
-					calculated_price: formatAmount({
-						amount: cheapestVariant.calculated_price,
-						region: region,
-						includeTaxes: false,
-					}),
-					original_price: formatAmount({
-						amount: cheapestVariant.original_price,
-						region: region,
-						includeTaxes: false,
-					}),
-					difference: getPercentageDiff(
-						cheapestVariant.original_price,
-						cheapestVariant.calculated_price
-					),
-					price_type: cheapestVariant.calculated_price_type,
-			  }
-			: undefined,
-	};
-};

0cf96d094649943065fdca274fb48fba67913112
samirjabib <samirdev9669@gmail.com>
2024-05-24 12:47:17 -0500

    config prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,26 @@ import { Poppins } from 'next/font/google';
 import './globals.css';
 
 const poppins_font = Poppins({
-	weight: ['400', '500', '700', '800', '900'],
-	style: ['normal', 'italic'],
-	subsets: ['latin'],
-	display: 'swap',
+  weight: ['400', '500', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://localhost:8000';
 
 export const metadata: Metadata = {
-	metadataBase: new URL(BASE_URL),
+  metadataBase: new URL(BASE_URL),
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body className={poppins_font.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={poppins_font.className}>{children}</body>
+    </html>
+  );
 }

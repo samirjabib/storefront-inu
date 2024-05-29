0f3985f0a5580591baac18633975180399e4b95b
samirjabib <samirdev9669@gmail.com>
2024-05-29 15:59:27 -0500

    work on seller page

--- src/components/home/promises/promises.tsx
+++ src/components/home/promises/promises.tsx
@@ -1,13 +1,13 @@
-import { Package } from "lucide-react";
+import { Package } from 'lucide-react';
 export default function Promises() {
   return (
     <section className="flex flex-row max-w-screen-xl px-4 md:px-6 mx-auto">
-      <div className="grid grid-cols-1 gap-5 w-full md:grid-cols-4 bg-blue-100 py-6 px-4 rounded-lg">
+      <div className="grid grid-cols-1 gap-5 w-full md:grid-cols-4 bg-brand-100 py-6 px-4 rounded-lg">
         {Array.from({ length: 4 }).map((item) => {
           return (
             <div className="flex items-row items-center flex-row gap-x-4 border border-gray-200 w-full p-3 rounded-lg bg-neutral-50">
-              <div className="bg-blue-200 rounded-full shadow-xs w-12 h-12 border border-blue-300 flex items-center justify-center">
-                <Package size={24} className="text-gray-700" />
+              <div className="bg-brand-100 rounded-full shadow-xs w-12 h-12 border border-brand-500 flex items-center justify-center">
+                <Package size={24} className="text-gray-500" />
               </div>
               <div>
                 <h3 className="text-xs text-gray-800 font-semibold">

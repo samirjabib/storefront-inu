707bc8deaf47108cd91d308c881b6257c72aee07
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-05-29 23:58:12 -0500

    fix addi icon

--- public/assets/assets.ts
+++ public/assets/assets.ts
@@ -4,8 +4,11 @@ import dog_food_3 from './dummy-product-images/alimento-dog-3.webp';
 
 import dummy_1920x1080 from './dummy-product-images/1980x1080.png';
 import max_banner from './hero-banners/max-banner.gif';
-
+import addi_icon from './payment-methods-logos/addi-icon.png';
+import addi from './payment-methods-logos/addi.svg';
 export const assets = {
+  addi,
+  addi_icon,
   dog_food_1,
   dog_food_2,
   dog_food_3,

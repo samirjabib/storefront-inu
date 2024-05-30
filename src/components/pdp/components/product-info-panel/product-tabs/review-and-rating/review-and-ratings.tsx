0656ce70f51d2c283afc2933680514b0ebbd8126
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-05-30 17:28:10 -0500

    added

--- src/components/pdp/components/product-info-panel/product-tabs/review-and-rating/review-and-ratings.tsx
+++ src/components/pdp/components/product-info-panel/product-tabs/review-and-rating/review-and-ratings.tsx
@@ -1,11 +1,14 @@
-import Rating from './rating';
+import ProductReview from '../../product-review';
+import ProductReviews from './product-reviews';
+import ProductRating from './product-rating';
 import ReviewWithPhotos from './reviews-with-photos';
 
 export default function ReviewAndRatings() {
   return (
     <div className="flex flex-col">
-      <Rating review_rating={4.9} />
+      <ProductRating review_rating={4.9} />
       <ReviewWithPhotos />
+      <ProductReviews />
     </div>
   );
 }

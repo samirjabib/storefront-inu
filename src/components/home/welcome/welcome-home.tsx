01bde5020d6adc611efe6ca7134ada2dac6d98d5
samirjabib <samirdev9669@gmail.com>
2024-05-29 20:04:06 -0500

    working

--- src/components/home/welcome/welcome-home.tsx
+++ src/components/home/welcome/welcome-home.tsx
@@ -1,10 +1,18 @@
+import { PawPrint } from 'lucide-react';
+
 export default function WelcomeHome() {
   return (
     <div className="py-12 px-4 ">
-      <h1 className="text-center text-neutral-900 font-bold text-3xl lg:text-5xl mb-4">
-        Bienvenidos a <span className="text-brand-500">Sho</span>
-        <span className="text-orange-300">pet</span>
-      </h1>
+      <div className="flex flex-row items-baseline gap-x-1 mb-4 justify-center">
+        <h1 className="text-center text-neutral-900 font-bold text-3xl lg:text-5xl">
+          Bienvenidos a{' '}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-brand-500 to-orange-300">
+            Shopet
+          </span>
+        </h1>
+        <PawPrint className="text-orange-300" size={20} />
+      </div>
+
       <p className="text-sm font-medium  text-center max-w-screen-md mx-auto">
         Somos un marketplace especializado en{' '}
         <span className="font-bold">Mascotas</span>. Seleccionamos a los mejores

10ab0556142273949609baf240b0cde78da7da8d
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-05-29 22:00:48 -0500

    added

--- public/assets/assets.ts
+++ public/assets/assets.ts
@@ -1,29 +1,14 @@
-import ps5 from './dummy-product-images/ps5.webp';
-import ssd from './dummy-product-images/ssd.webp';
-import airpods from './dummy-product-images/airpods.webp';
-import gafas from './dummy-product-images/gafas.webp';
-import iphone from './dummy-product-images/iphone.webp';
-import smart_home from './dummy-product-images/home.webp';
-import addi_icon from './payment-methods-logos/addi-icon.png';
-import addi from './payment-methods-logos/addi.svg';
-
 import dog_food_1 from './dummy-product-images/alimento-dog-1.webp';
 import dog_food_2 from './dummy-product-images/alimento-dog-2.png';
 import dog_food_3 from './dummy-product-images/alimento-dog-3.webp';
 
 import dummy_1920x1080 from './dummy-product-images/1980x1080.png';
+import max_banner from './hero-banners/MAX-BANNER.jpg';
 
 export const assets = {
-	ps5,
-	ssd,
-	airpods,
-	dog_food_1,
-	dog_food_2,
-	dog_food_3,
-	dummy_1920x1080,
-	gafas,
-	iphone,
-	smart_home,
-	addi,
-	addi_icon,
+  dog_food_1,
+  dog_food_2,
+  dog_food_3,
+  dummy_1920x1080,
+  max_banner,
 };

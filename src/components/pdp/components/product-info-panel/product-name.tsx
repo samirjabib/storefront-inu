4adcf6c95f6cc943f0db89cf0ff2731feb4feea8
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-05-31 00:59:35 -0500

    added

--- src/components/pdp/components/product-info-panel/product-name.tsx
+++ src/components/pdp/components/product-info-panel/product-name.tsx
@@ -1,6 +1,6 @@
 export default function ProductName() {
   return (
-    <h1 className="text-2xl lg:text-3xl font-bold">
+    <h1 className="text-2xl lg:text-3xl font-extrabold tracking-tight mb-2">
       Alimento Perro Max Vita Adultos Buffet Pollo Y Vegetales - 2 Kg
     </h1>
   );

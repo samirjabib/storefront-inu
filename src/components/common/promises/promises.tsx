784204fc0a3235eb7f4672c70212794960e9b155
samirjabib <samirdev9669@gmail.com>
2024-06-04 19:19:33 -0500

    change icons

--- src/components/common/promises/promises.tsx
+++ src/components/common/promises/promises.tsx
@@ -1,5 +1,29 @@
 import { cn } from '@/lib/utils';
-import { Package } from 'lucide-react';
+import { Package, CreditCard, Truck, TicketPercent } from 'lucide-react';
+
+const promises = [
+  {
+    icon: <Truck size={24} className="text-emerald-500" />,
+    title: 'Entregas el mismo día',
+    description: 'Entrega en 24 horas hábiles',
+  },
+  {
+    icon: <Package size={24} className="text-emerald-500" />,
+    title: 'Devoluciones sin costo',
+    description: 'Hasta 30 días para devolver',
+  },
+  {
+    icon: <CreditCard size={24} className="text-emerald-500" />,
+    title: 'Pago seguro',
+    description: 'Pasarelas de pago certificadas y seguras',
+  },
+  {
+    icon: <TicketPercent size={24} className="text-emerald-500" />,
+    title: 'Descuentos exclusivos',
+    description: 'Ofertas solo para miembros',
+  },
+];
+
 export default function Promises({
   className,
   classNameContainer,
@@ -20,21 +44,22 @@ export default function Promises({
           className
         )}
       >
-        {Array.from({ length: 4 }).map((item) => {
-          return (
-            <div className="flex items-row items-center flex-row gap-x-4 border border-gray-200 w-full p-3 rounded-lg bg-neutral-50">
-              <div className="bg-brand-50 rounded-full shadow-xs w-12 h-12 border border-brand-300 flex items-center justify-center">
-                <Package size={24} className="text-emerald-500" />
-              </div>
-              <div>
-                <h3 className="text-xs text-gray-800 font-semibold">
-                  Envío gratis en medellin Colombia
-                </h3>
-                <p className="text-xs text-gray-500">Entrega en 24h hábiles</p>
-              </div>
+        {promises.map((item, index) => (
+          <div
+            key={index}
+            className="flex items-row items-center flex-row gap-x-4 border border-gray-200 w-full p-3 rounded-lg bg-neutral-50"
+          >
+            <div className="bg-brand-50 rounded-full shadow-xs w-12 h-12 border border-brand-300 flex items-center justify-center">
+              {item.icon}
             </div>
-          );
-        })}
+            <div>
+              <h3 className="text-xs text-gray-800 font-semibold">
+                {item.title}
+              </h3>
+              <p className="text-xs text-gray-500">{item.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );

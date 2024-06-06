98e53ad0cc903a6f793aecec7f3f9ca5c383f26f
samirjabib <samirdev9669@gmail.com>
2024-06-06 07:35:28 -0500

    mynors fixs header

--- src/components/layout/header/search.tsx
+++ src/components/layout/header/search.tsx
@@ -12,7 +12,7 @@ export default function Search({ className }: { className?: string }) {
     >
       <Input
         placeholder="¿Que buscas?"
-        className="rounded-full bg-background text-sm md:text-base drop-shadow-2xl"
+        className="rounded-full bg-background text-sm md:text-base drop-shadow-md"
       />
       <div className="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-end pr-4">
         <SearchIcon className="text-muted-foreground" size={24} />

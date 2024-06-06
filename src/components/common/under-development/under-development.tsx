53a2a741587ce632e7c6b033cf73f66730fd4242
samirjabib <samirdev9669@gmail.com>
2024-06-05 21:36:31 -0500

    enable categorys

--- src/components/common/under-development/under-development.tsx
+++ src/components/common/under-development/under-development.tsx
@@ -3,7 +3,7 @@ export default function UnderDevelopment() {
     <div className="w-full p-4 bg-yellow-500">
       <div className="bg-black p">
         <p className="text-red-500 font-semibold flex gap-2 justify-center animate-pulse">
-          This web is under development by Arrecife
+          This web is under development
           <span className="text-red-500">&lt;/&gt;</span>
         </p>
       </div>

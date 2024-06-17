37571e876e834d9b9da62e6b6130a66171376290
samirjabib <samirdev9669@gmail.com>
2024-06-17 13:42:15 -0500

    mynor fixs

--- src/components/layout/header/side-menu/side-menu-navigation.tsx
+++ src/components/layout/header/side-menu/side-menu-navigation.tsx
@@ -1,52 +1,28 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import SideBarCategoryTreeAccordion from './side-bar-category-tree-accordion';
 import { ProductCategoryWithChildren } from '@/lib/types/global';
-import { ArrowLeft } from 'lucide-react';
+import { ChevronLeft } from 'lucide-react';
+import useMenuCategories from '../hooks/useMenuCategories';
 
 const SideMenuNavigation = ({
   product_categories,
 }: {
   product_categories: ProductCategoryWithChildren[];
 }) => {
-  const [categories, setCategories] = useState<ProductCategoryWithChildren[]>(
-    []
-  );
-
-  const [currentCategory, setCurrentCategory] =
-    useState<ProductCategoryWithChildren | null>(null);
-  const [categoryStack, setCategoryStack] = useState<
-    ProductCategoryWithChildren[]
-  >([]);
-
-  useEffect(() => {
-    setCategories(product_categories);
-  }, [product_categories]);
-
-  const handleSelectCategory = (category: ProductCategoryWithChildren) => {
-    if (currentCategory) {
-      setCategoryStack([...categoryStack, currentCategory]);
-    }
-    setCurrentCategory(category);
-  };
-
-  const handleBack = () => {
-    const newStack = [...categoryStack];
-    const previousCategory = newStack.pop() || null;
-    setCategoryStack(newStack);
-    setCurrentCategory(previousCategory);
-  };
-
-  const categoriesToDisplay = currentCategory
-    ? currentCategory.category_children
-    : categories;
+  const {
+    categoriesToDisplay,
+    handleBack,
+    handleSelectCategory,
+    currentCategory,
+  } = useMenuCategories({ product_categories });
 
   return (
     <div>
       {currentCategory && (
         <div className="py-4 border-b w-full" onClick={handleBack}>
           <button className="capitalize text-sm text-primary-foreground/80 cursor-pointer flex flex-row items-center gap-x-1">
-            <ArrowLeft className="text-black opacity-60" />
+            <ChevronLeft className="text-black opacity-60" size={24} />
             Volver
           </button>
         </div>

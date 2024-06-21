bb7d54a91f3ad37ded1a3353775a73fecb350f68
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2024-06-20 23:50:58 -0500

    fix localized clientLink

--- src/components/layout/tabs/tabs.tsx
+++ src/components/layout/tabs/tabs.tsx
@@ -1,4 +1,6 @@
+import LocalizedClientLink from '@/components/common/localized-client-link/localized-client-link';
 import { Home as HomeIcon, Search, UserRound, PawPrint } from 'lucide-react';
+import Link from 'next/link';
 
 const categories = [
   { icon: HomeIcon, label: 'Inicio' },
@@ -11,7 +13,8 @@ export default function Tabs() {
   return (
     <div className="fixed md:hidden bottom-0 left-0 w-full bg-neutral-100 flex flex-row justify-between p-4 shadow-lg border-t border-neutral-300 z-40">
       {categories.map((item) => (
-        <div
+        <LocalizedClientLink
+          href={'/account'}
           key={item.label}
           className="items-center flex flex-col justify-center cursor-pointer"
         >
@@ -19,7 +22,7 @@ export default function Tabs() {
           <h3 className="text-neutral-500 text-xs transition-colors duration-200 hover:text-brand-700 mt-1">
             {item.label}
           </h3>
-        </div>
+        </LocalizedClientLink>
       ))}
     </div>
   );
